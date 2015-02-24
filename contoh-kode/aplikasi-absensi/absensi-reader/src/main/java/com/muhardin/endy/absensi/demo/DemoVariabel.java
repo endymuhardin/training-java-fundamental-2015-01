180b145687d5bc31bed66263b67b729c957da41f
Endy Muhardin <endy.muhardin@gmail.com>
2015-02-24 09:45:13 +0700

    contoh penggunaan access modifier

---
+++ contoh-kode/aplikasi-absensi/absensi-reader/src/main/java/com/muhardin/endy/absensi/demo/DemoVariabel.java
@@ -0,0 +1,14 @@
+package com.muhardin.endy.absensi.demo;
+
+import com.muhardin.endy.absensi.Karyawan;
+
+public class DemoVariabel {
+    
+    public static void main(String[] x){
+
+        Karyawan k = new Karyawan();
+
+        System.out.println("ID : "+k.id);
+        System.out.println("Nama : "+k.nama);
+    }
+}
