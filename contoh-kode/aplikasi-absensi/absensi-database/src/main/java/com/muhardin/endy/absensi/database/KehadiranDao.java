73edf5364ed5a4d95dd013ff90c9097decdf179f
Endy Muhardin <endy.muhardin@gmail.com>
2015-02-26 09:53:07 +0700

    template method untuk akses tabel kehadiran

--- contoh-kode/aplikasi-absensi/absensi-database/src/main/java/com/muhardin/endy/absensi/database/KehadiranDao.java
+++ contoh-kode/aplikasi-absensi/absensi-database/src/main/java/com/muhardin/endy/absensi/database/KehadiranDao.java
@@ -1,9 +1,18 @@
 package com.muhardin.endy.absensi.database;
 
+import com.muhardin.endy.absensi.Karyawan;
 import com.muhardin.endy.absensi.Kehadiran;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KehadiranDao {
     public void simpan(Kehadiran k){
         
+    }
+    
+    public List<Kehadiran> cariKehadiran(Karyawan k){
+        List<Kehadiran> hasil = new ArrayList<>();
+        
+        return hasil;
     }
 }
